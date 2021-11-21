a6ff26dfdbb114cb9a2f50764d5e70b63e14108f
JGuo <jason.guo2560@gmail.com>
2021-11-21 00:43:48 -0500

    v0.1.2 - updated splash

--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -1,8 +1,36 @@
+import Button from '@mui/material/Button';
+
 export default function SplashScreen() {
     return (
         <div id="splash-screen">
-            The Top 5<br />
-            Lister
+            <h1>Top 5 Lister</h1>
+            <p>Post and share your top 5 lists with others</p>
+            <div>
+                <Button
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    style={{ width: '230px', height: '30px', fontSize: '11px' }}
+                    href='/login/'
+                >
+                    Login
+                </Button>
+                <Button
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    style={{ width: '230px', height: '30px', fontSize: '11px' }}
+                    href='/register/'
+                >
+                    Create New Account
+                </Button>
+                <Button
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    style={{ width: '230px', height: '30px', fontSize: '11px' }}
+                >
+                    Login as Guest
+                </Button>
+            </div>
+            <span>Developed By Jason Guo</span>
         </div>
     )
 }
