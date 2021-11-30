96e9f55863ddab9293067f4980fee83f093f5c05
JGuo <jason.guo2560@gmail.com>
2021-11-30 18:01:00 -0500

    v0.11.1 - updated publish date format

--- client/src/components/ListCard.js
+++ client/src/components/ListCard.js
@@ -29,6 +29,48 @@ function ListCard(props) {
     const [listOpened, setListOpened] = useState(false);
     const { listInfo } = props;
     let location = useLocation();
+    let publishDate = "";
+    let datePieces = listInfo.published.split('-')
+    console.log(datePieces)
+    switch (datePieces[1]) {
+        case '01':
+            publishDate = "Jan";
+            break;
+        case '02':
+            publishDate = "Feb";
+            break;
+        case '03':
+            publishDate = "Mar";
+            break;
+        case '04':
+            publishDate = "Apr";
+            break;
+        case '05':
+            publishDate = "May";
+            break;
+        case '06':
+            publishDate = "Jun";
+            break;
+        case '07':
+            publishDate = "Jul";
+            break;
+        case '08':
+            publishDate = "Aug";
+            break;
+        case '09':
+            publishDate = "Sep";
+            break;
+        case '10':
+            publishDate = "Oct";
+            break;
+        case '11':
+            publishDate = "Nov";
+            break;
+        case '12':
+            publishDate = "Dec";
+            break;
+    }
+    publishDate += (' ' + datePieces[2] + ', ' + datePieces[0]);
 
     function handleEditList(event, id) {
         if (!event.target.disabled) {
@@ -80,23 +122,23 @@ function ListCard(props) {
             <div className='list-card-header'>
                 <Box sx={{ p: 1 }}>
                     <Typography fontWeight='bold'>{listInfo.name}</Typography>
-                    <Typography sx={{fontSize: 14}}>By: {<span>{listInfo.owner}</span>}</Typography>
+                    <Typography sx={{ fontSize: 14 }}>By: {<span>{listInfo.owner}</span>}</Typography>
                 </Box>
                 <Box sx={{ p: 1, display: 'flex', alignItems: 'center' }}>
                     <IconButton className={auth.user ? listInfo.likes.includes(auth.user.username) ? 'is-on' : null : null} onClick={(event) => {
                         handleLikeList(event, listInfo._id)
-                    }} aria-label='like' disabled={auth.user ? auth.user.username === ' ' || auth.user.username === listInfo.owner : false } >
-                        {auth.user ? listInfo.likes.includes(auth.user.username) ? 
-                        <ThumbUpIcon style={{ fontSize: '24pt' }} /> 
-                        : <ThumbUpOffAltIcon style={{ fontSize: '24pt' }} /> : <ThumbUpOffAltIcon style={{ fontSize: '24pt' }} />}
+                    }} aria-label='like' disabled={auth.user ? auth.user.username === ' ' || auth.user.username === listInfo.owner : false} >
+                        {auth.user ? listInfo.likes.includes(auth.user.username) ?
+                            <ThumbUpIcon style={{ fontSize: '24pt' }} />
+                            : <ThumbUpOffAltIcon style={{ fontSize: '24pt' }} /> : <ThumbUpOffAltIcon style={{ fontSize: '24pt' }} />}
                     </IconButton>
                     <Typography>{listInfo.likes.length}</Typography>
                     <IconButton onClick={(event) => {
                         handleDislikeList(event, listInfo._id)
-                    }} aria-label='dislike' disabled={auth.user ? auth.user.username === ' ' || auth.user.username === listInfo.owner : false } >
-                        {auth.user ? listInfo.dislikes.includes(auth.user.username) ? 
-                        <ThumbDownIcon style={{ fontSize: '24pt' }} /> 
-                        : <ThumbDownOffAltIcon style={{ fontSize: '24pt' }} /> : <ThumbDownOffAltIcon style={{ fontSize: '24pt' }} />}
+                    }} aria-label='dislike' disabled={auth.user ? auth.user.username === ' ' || auth.user.username === listInfo.owner : false} >
+                        {auth.user ? listInfo.dislikes.includes(auth.user.username) ?
+                            <ThumbDownIcon style={{ fontSize: '24pt' }} />
+                            : <ThumbDownOffAltIcon style={{ fontSize: '24pt' }} /> : <ThumbDownOffAltIcon style={{ fontSize: '24pt' }} />}
                     </IconButton>
                     <Typography>{listInfo.dislikes.length}</Typography>
                     {auth.user.username === listInfo.owner && location.pathname === '/' ? <IconButton onClick={(event) => {
@@ -110,16 +152,18 @@ function ListCard(props) {
             <div className='list-card-footer'>
                 <Box sx={{ p: 1, marginTop: '10px' }}>
                     {listInfo.published !== '1970-01-01' ?
-                        <Typography sx={{fontSize: 14, fontWeight: 'bold'}}>Published: <span style={{color: '#79ae5a'}}>{listInfo.published}</span></Typography>
-                        : <Button sx={{ padding: '0', color: 'red', textDecoration: 'underline' ,
-                        ':hover': { bgcolor: 'transparent', color: 'red', boxShadow: 'none', textDecoration: 'underline', fontWeight: 'bold' }}} 
-                        onClick={(event) => { handleEditList(event, listInfo._id) }} 
-                        aria-label='edit'>
+                        <Typography sx={{ fontSize: 14, fontWeight: 'bold' }}>Published: <span style={{ color: '#79ae5a' }}>{publishDate}</span></Typography>
+                        : <Button sx={{
+                            padding: '0', color: 'red', textDecoration: 'underline',
+                            ':hover': { bgcolor: 'transparent', color: 'red', boxShadow: 'none', textDecoration: 'underline', fontWeight: 'bold' }
+                        }}
+                            onClick={(event) => { handleEditList(event, listInfo._id) }}
+                            aria-label='edit'>
                             Edit
                         </Button>}
                 </Box>
                 <Box sx={{ p: 1, display: 'flex', alignItems: 'center' }}>
-                    <Typography sx={{fontSize: 14}}>Views: <span style={{color: '#972c24', fontWeight: 'bold'}}>{listInfo.views}</span></Typography>
+                    <Typography sx={{ fontSize: 14 }}>Views: <span style={{ color: '#972c24', fontWeight: 'bold' }}>{listInfo.views}</span></Typography>
                     {listOpened ?
                         <IconButton style={{ padding: '0' }} onClick={(event) => {
                             handleCloseList(event)
